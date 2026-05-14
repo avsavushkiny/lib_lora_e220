c882c5018ece9d8d6d302f2ea5794eb7a1607314
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2026-05-14 18:05:50 +0300

    Update src.ino
    rover edition

--- src/src.ino
+++ src/src.ino
@@ -23,6 +23,60 @@ E220Config config;
 unsigned long previousTime = 0;
 const long interval = 10000; // 10
 
+// ---------- ROVER functions ----------
+
+const int in2 = 10; // m1
+const int in1 = 9;  // m1
+
+const int in4 = 12; // m2
+const int in3 = 11; // m2
+
+void setupPinRover()
+{
+    pinMode(in1, 0x1); pinMode(in2, 0x1); // m1
+    pinMode(in3, 0x1); pinMode(in4, 0x1); // m2
+}
+
+void testMotorRover()
+{
+    digitalWrite(in1, 0x1); digitalWrite(in2, 0x0); // backward m1
+    digitalWrite(in4, 0x1); digitalWrite(in3, 0x0); // backward m2
+    delay(5000);
+    
+    digitalWrite(in1, 0x0); digitalWrite(in2, 0x1); // forward m1
+    digitalWrite(in4, 0x0); digitalWrite(in3, 0x1); // forward m2
+    delay(5000);
+}
+
+void forwardRover()
+{
+    digitalWrite(in1, 0x0); digitalWrite(in2, 0x1); // forward m1
+    digitalWrite(in4, 0x0); digitalWrite(in3, 0x1); // forward m2
+}
+
+void backwardRover()
+{
+    digitalWrite(in1, 0x1); digitalWrite(in2, 0x0); // backward m1
+    digitalWrite(in4, 0x1); digitalWrite(in3, 0x0); // backward m2
+}
+
+void leftRover()
+{
+
+}
+
+void rightRover()
+{
+
+}
+
+void stopRover()
+{
+    digitalWrite(in1, 0x0); digitalWrite(in2, 0x0); // backward m1
+    digitalWrite(in4, 0x0); digitalWrite(in3, 0x0); // backward m2
+}
+// ---------- END ROVER functions ---------
+
 void setup()
 {
     Serial.begin(115200);
@@ -61,27 +115,40 @@ void setup()
     }
 
     Serial.println("Готов к работе!");
+
+    // --------- rover ---------
+    setupPinRover();
+    // testMotorRover();
 }
 
 void loop()
 {
-    // Отправляем SAY HELLO каждые 10 секунд
-    unsigned long currentTime = millis();
+    // // Отправляем SAY HELLO каждые 10 секунд
+    // unsigned long currentTime = millis();
 
-    if (currentTime - previousTime >= interval) {
-        sendSayHello();
-        previousTime = currentTime;
-    }
+    // if (currentTime - previousTime >= interval) {
+    //     sendSayHello();
+    //     previousTime = currentTime;
+    // }
     
     // Проверяем полученные данные
     if (lora.available())
     {
         String msg = lora.receiveString();
-        Serial.println("RECEIVED");
+        Serial.print("R: ");
         Serial.println(msg);
 
         // // Отправляем ответ
         // lora.sendString(msg);
+
+        if (msg == "W\n")
+        {
+            forwardRover(); delay(1000); msg = ""; stopRover();
+        }
+        if (msg == "S\n")
+        {
+            backwardRover(); delay(1000); msg = ""; stopRover();
+        }
     }
 
     // Отправляем данные по Serial
@@ -90,11 +157,11 @@ void loop()
         String msg = Serial.readString();
         if (lora.sendString(msg))
         {
-            Serial.println("SEND");
+            // Serial.println("SEND");
         }
         else
         {
-            Serial.println("ERROR");
+            // Serial.println("ERROR");
         }
     }
 }
@@ -103,3 +170,4 @@ bool sendSayHello()
 {
     lora.sendStringWithAddress("Hello", 0x1234, 23);
 }
+
